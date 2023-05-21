0d09d6084a789c9f528e38584e53d67a39cfecf4
satyam575 <satyamsinghal575@gmail.com>
2023-05-21 19:12:26 +0530

    validations on add player input

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,137 +1,175 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, FlatList,Pressable ,ScrollView , Image} from 'react-native';
-import { Ionicons } from '@expo/vector-icons'
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  FlatList,
+  Pressable,
+  ScrollView,
+  Image,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-import UserItem from '../components/userItem';
+import UserItem from "../components/userItem";
 
 function HomeScreen({ navigation }) {
-  
-    const [player, onChangePlayer] = useState('');
-    const [playersArray, onChangePlayersArray] = useState([]);
+  const [player, onChangePlayer] = useState("");
+  const [playersArray, onChangePlayersArray] = useState([]);
 
-    const removePlayerFromArray = (playerToRemove)=>{
-        onChangePlayersArray(playersArray.filter((item) => item != playerToRemove))
+  const removePlayerFromArray = (playerToRemove) => {
+    onChangePlayersArray(playersArray.filter((item) => item != playerToRemove));
+  };
+
+  function playerAddPress() {
+    if (
+      player == "" ||
+      playersArray.filter((x) => x.name == player).length != 0
+    ) {
+      return;
     }
-
-    return (
-      <View style={styles.container}>
-        <View style = {{marginTop:30,alignContent:"center",alignItems:"center",marginBottom:30}}> 
-            <Image
-            style={styles.tinyLogo}
-            source={require('../../assets/icon.png')}/>
-            <Text style = {{fontSize : 30, color:"#00796B",fontWeight:"bold"}}>Liquor League</Text>
-        </View>
-        <ScrollView>
-        <FlatList style = {styles.list}
-          horizontal  
-          data ={playersArray}
-          contentContainerStyle={styles.listContents}
-          renderItem={({item})=>
-                <UserItem item={item} removePlayer = {removePlayerFromArray}></UserItem>
-        }
-          keyExtractor={item => item.name}
-        >
-        </FlatList>
-        </ScrollView>
-        
-        <View style ={styles.inputContainer}>
-            <TextInput
-            style={styles.input}
-            onChangeText={onChangePlayer}
-            value={player}
-            placeholder="Add Player"
-            autoFocus={true}
-            onSubmitEditing = {()=>{onChangePlayersArray(arr => [...arr,{"name":player}]) ; onChangePlayer("") ;}}
-            />
-            <Ionicons name="add-circle" size={30} color="#00796B" onPress={()=>{onChangePlayersArray(arr => [...arr,{"name":player}]) ; onChangePlayer("") ;}}/>
-        </View>
-        
-        <Pressable style= {styles.GameOn} onPress={()=>{navigation.navigate('GameScreen',{
-          "players":playersArray
-        })}}>
-
-            <Text style={styles.GameOnText}>Game On</Text>
-        
-        </Pressable>
-      </View>
-    );
+    onChangePlayersArray((arr) => [...arr, { name: player }]);
+    onChangePlayer("");
   }
 
-  const styles = StyleSheet.create({
-    inputContainer:{
-        minHeight:60,
-        width: '90%',
-        height: 60,
-        maxHeight : 60,
-        margin: 12,
-        flex:1,
-        borderWidth: 2,
-        margin:30,
-        borderColor:"#00796B",
-      borderRadius:7,
-      flexDirection:"row",
-      alignContent:"center",
-        alignItems:"center",
-        paddingRight:15
-    },
-    tinyLogo: {
-      width: 150,
-      height: 150,
-    },
-    input: {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          marginTop: 30,
+          alignContent: "center",
+          alignItems: "center",
+          marginBottom: 30,
+        }}
+      >
+        <Image
+          style={styles.tinyLogo}
+          source={require("../../assets/icon.png")}
+        />
+        <Text style={{ fontSize: 30, color: "#00796B", fontWeight: "bold" }}>
+          Liquor League
+        </Text>
+      </View>
+      <ScrollView>
+        <FlatList
+          style={styles.list}
+          horizontal
+          data={playersArray}
+          contentContainerStyle={styles.listContents}
+          renderItem={({ item }) => (
+            <UserItem
+              item={item}
+              removePlayer={removePlayerFromArray}
+            ></UserItem>
+          )}
+          keyExtractor={(item) => item.name}
+        ></FlatList>
+      </ScrollView>
 
-      fontWeight: 'bold',
-      fontSize:23,
-      flex:1,
-      padding: 10,
-      color:"#00796B",
-      
-    },
-    GameOn: {
-        width: '90%',
-        minHeight:60,
-        height: 60,
-        maxHeight : 60,
-        margin: 12,
-        marginTop:0,
-        flex:1,
-        borderWidth: 2,
-        fontWeight: 'bold',
-        fontSize:23,
-        borderColor:"#00796B",
-        borderRadius:7,
-        padding: 10,
-        color:"#00796B",
-        alignContent:"center",
-        alignItems:"center"
-      },
-      GameOnText: {
-        fontWeight: 'bold',
-        fontSize:23,
-        color:"#00796B",
-      },
-    container: {
-      flex: 1,
-      backgroundColor: '#FFEB3B',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding :30,
-      paddingHorizontal:15
-    },
-    list: {
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangePlayer}
+          value={player}
+          placeholder="Add Player"
+          autoFocus={true}
+          onSubmitEditing={() => {}}
+        />
+        <Ionicons
+          name="add-circle"
+          size={30}
+          color="#00796B"
+          onPress={playerAddPress}
+        />
+      </View>
+
+      <Pressable
+        style={styles.GameOn}
+        onPress={() => {
+          navigation.navigate("GameScreen", {
+            players: playersArray,
+          });
+        }}
+      >
+        <Text style={styles.GameOnText}>Game On</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    minHeight: 60,
+    width: "90%",
+    height: 60,
+    maxHeight: 60,
+    margin: 12,
+    flex: 1,
+    borderWidth: 2,
+    margin: 30,
+    borderColor: "#00796B",
+    borderRadius: 7,
+    flexDirection: "row",
+    alignContent: "center",
+    alignItems: "center",
+    paddingRight: 15,
+  },
+  tinyLogo: {
+    width: 150,
+    height: 150,
+  },
+  input: {
+    fontWeight: "bold",
+    fontSize: 23,
+    flex: 1,
+    padding: 10,
+    color: "#00796B",
+  },
+  GameOn: {
+    width: "90%",
+    minHeight: 60,
+    height: 60,
+    maxHeight: 60,
+    margin: 12,
+    marginTop: 0,
+    flex: 1,
+    borderWidth: 2,
+    fontWeight: "bold",
+    fontSize: 23,
+    borderColor: "#00796B",
+    borderRadius: 7,
+    padding: 10,
+    color: "#00796B",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  GameOnText: {
+    fontWeight: "bold",
+    fontSize: 23,
+    color: "#00796B",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#FFEB3B",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 30,
+    paddingHorizontal: 15,
+  },
+  list: {
     //   padding: 10,
-      flexDirection: 'column',
-      width:"100%",
+    flexDirection: "column",
+    width: "100%",
     //   backgroundColor:"black"
-    },
-    listContents: {
-      flexDirection: 'row',
-      width: '100%',
-      flexWrap: 'wrap',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding:9
-    },
-  });
+  },
+  listContents: {
+    flexDirection: "row",
+    width: "100%",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 9,
+  },
+});
 
-  export default HomeScreen
+export default HomeScreen;
