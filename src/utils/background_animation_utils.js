9f6c563b61e564d9907a4022cb559461ecd7804e
satyam575 <satyamsinghal575@gmail.com>
2023-05-25 22:41:22 +0530

    fixing animation for gamescreen

--- src/utils/background_animation_utils.js
+++ src/utils/background_animation_utils.js
@@ -1,6 +1,6 @@
 export const INPUT_RANGE_START = 0;
 export const INPUT_RANGE_END = 1;
-export const OUTPUT_RANGE_START = -281;
+export const OUTPUT_RANGE_START = -200;
 export const OUTPUT_RANGE_END = 0;
 export const ANIMATION_TO_VALUE = 1;
 export const ANIMATION_DURATION = 25000;
